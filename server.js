19b3a9f7e7345c987b195b8c1fd5a791d5ea2af1
Your Name <you@example.com>
2019-09-06 13:30:05 -0400

    done GET actions

--- server.js
+++ server.js
@@ -51,6 +51,9 @@ server.get('/api/projects/:id', (req, res) => {
                 res.status(200).json(project)
             }
             )
+            .catch(err => {
+                res.status(500).json({message: "failed to get projects"})
+            })
 })
 
 //PUT PROJECT: '/api/projects/:id'
@@ -98,6 +101,15 @@ server.delete('/api/projects/:id', (req,res) =>{
         
 })
 
-//GET ACTION
-server.get('./')
+//GET ACTIONS
+server.get('/api/projects/:id/actions', (req,res) => {
+    projectData
+            .getProjectActions(req.params.id)
+            .then(actions => {
+                res.status(200).json(actions)
+            })
+            .catch(err => {
+                res.status(500).json({message: "failed to get actions"})
+            })
+})
 
