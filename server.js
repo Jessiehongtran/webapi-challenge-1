f209b6b85fa445db94acefb4cc5ad4eeda8f8fd9
Your Name <you@example.com>
2019-09-06 11:58:41 -0400

    done GET projects and got projects

--- server.js
+++ server.js
@@ -10,3 +10,18 @@ server.use(express.json())
 server.get('/', (req, res) => {
   res.send(`<h2>Let's rock!</h2>`)
 });
+
+const projectData = require('./data/helpers/projectModel')
+
+//GET: '/api/projects'
+server.get('/api/projects', (req, res) => {
+    projectData
+            .get()
+            .then(projects => {
+                console.log(projects)
+                res.status(200).json(projects)
+            })
+    
+})
+
+//POST: '/api/projects'
