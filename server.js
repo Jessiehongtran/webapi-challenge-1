cc145e8809d257e4cf29a755c10870848525684a
Your Name <you@example.com>
2019-09-06 12:13:18 -0400

    done POST projects and got it work

--- server.js
+++ server.js
@@ -24,4 +24,21 @@ server.get('/api/projects', (req, res) => {
     
 })
 
+
 //POST: '/api/projects'
+server.post('/api/projects', (req,res) => {
+    const newProject = req.body;
+    // const {id} = req.params
+    // console.log(id)
+    const project = {
+        // id: id,
+        name: newProject.name,
+        description: newProject.description,
+        completed: newProject.completed
+    }
+    console.log(project)
+    projectData
+            .insert(project)
+            .then(newId => res.status(200).json(newId))
+            .catch(err => res.status(500).json({message: "failed to add project"}))
+})
